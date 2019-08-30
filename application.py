464aa3d1c114b3c77a11998e8a251767aa0c3946
Krishna R <krishnarao.2208@gmail.com>
2019-08-30 09:23:49 +0000

    Added registration feature as well as database. Login partially implemented.

--- application.py
+++ application.py
@@ -25,6 +25,51 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+db = SQL("sqlite:///plastic.db")
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        if not request.form.get("username"):
+            return("You must enter a username!")
+        elif not request.form.get("password"):
+            return("You must enter a password!")
+        else:
+            # Validate user
+            rows = db.execute("SELECT * from users WHERE username = :username", username=request.form.get("username"))
+            if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+                return "Invalid username or password!"
+            else:
+                session["user_id"] = rows[0]["id"]
+                return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        if not request.form.get("username"):
+            return("You must enter a username")
+        elif not request.form.get("password"):
+            return("You must enter a password")
+        elif not request.form.get("password_confirm"):
+            return("You must enter a password")
+        else:
+            if len(db.execute("SELECT username FROM users WHERE username=:username", username=request.form.get("username"))) != 0:
+                return("This username is already taken!")
+            elif request.form.get("password") != request.form.get("password_confirm"):
+                return("Your passswords did not match!")
+            else:
+                pw_hash=generate_password_hash(request.form.get("password"))
+                print(request.form.get("lang"))
+                db.execute("INSERT INTO users (username, hash, level, lang) VALUES (:username, :pw, 0, :lang)", username=request.form.get("username"), pw=pw_hash, lang=request.form.get("lang"))
+                return redirect("/")
+
+
